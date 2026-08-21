87fcbf525269b2c24fced9bd02ebd09870b2824c
Lovemore Munetsi <lovemoremunetsi@icloud.com>
2026-08-22 02:05:47 +0800

    Initial commit: Spring Boot starter (Web, JPA, Thymeleaf, Security, Validation, H2)

---
+++ src/test/java/com/lovemore/devprofile/DevprofileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovemore.devprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevprofileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
